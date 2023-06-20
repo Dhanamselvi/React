8b4a96ff108eec960c2409010c3829e006dab146
Selvi <selviace92@gmail.com>
2023-06-20 12:00:06 +0530

    updated the Id property

--- App.js
+++ App.js
@@ -1,3 +1,3 @@
-const headingReact = React.createElement('h1',{},"Namaste React")
+const headingReact = React.createElement('h1',{id:"hello"},"Namaste React")
 const root1 = ReactDOM.createRoot(document.getElementById("root"));
 root1.render(headingReact)
