5adcfb628af923cbfb44f46b4a3d94fc638b2068
Selvi <selviace92@gmail.com>
2023-06-21 22:22:25 +0530

    imported the React and ReactDom in App.js

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import React from "react"; import ReactDOM from "react-dom/client";
 const headingReact = React.createElement('h1',{id:"hello"},"Namaste React")
 const root1 = ReactDOM.createRoot(document.getElementById("root"));
 root1.render(headingReact)
