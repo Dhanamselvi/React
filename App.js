6d4e8bd646fcfb1673bc5bcd54b81c84836b42e4
Selvi <selviace92@gmail.com>
2023-06-22 07:10:41 +0530

    updated the app

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
-import React from "react"; import ReactDOM from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
 const headingReact = React.createElement('h1',{id:"hello"},"Namaste React")
 const root1 = ReactDOM.createRoot(document.getElementById("root"));
 root1.render(headingReact)
