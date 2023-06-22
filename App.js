756b67b4c13552b9056fb129b0c5d0b5ef17fead
Selvi <selviace92@gmail.com>
2023-06-22 12:20:58 +0530

    Created the list using React.createElement in App.js

--- App.js
+++ App.js
@@ -1,5 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-const headingReact = React.createElement('h1',{id:"hello"},"Namaste React")
+
+const headingReact = React.createElement(
+    'div',
+    {id:"hello"},
+    [
+        React.createElement(
+            'h1',
+            {key:"heading"},
+            "ReactJs"
+        ),
+        React.createElement(
+            'ul',
+            {key:"unorderedList"},
+            [
+                React.createElement(
+                    'li',
+                    {key:"about"},
+                    "About Us"
+                ),
+                React.createElement(
+                    'li',
+                    {key:"support"},
+                    "Support Us"
+                ),
+            ]
+        )
+    ])
 const root1 = ReactDOM.createRoot(document.getElementById("root"));
 root1.render(headingReact)
