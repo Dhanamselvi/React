2d717e950c628b8c98cb8aa8a84602130311e051
Selvi <selviace92@gmail.com>
2023-06-24 04:52:38 +0530

    added the babel transforn plugin

--- App.js
+++ App.js
@@ -1,31 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const headingReact = React.createElement(
-    'div',
-    {id:"hello"},
-    [
-        React.createElement(
-            'h1',
-            {key:"heading"},
-            "ReactJs"
-        ),
-        React.createElement(
-            'ul',
-            {key:"unorderedList"},
-            [
-                React.createElement(
-                    'li',
-                    {key:"about"},
-                    "About Us"
-                ),
-                React.createElement(
-                    'li',
-                    {key:"support"},
-                    "Support Us"
-                ),
-            ]
-        )
-    ])
+// Functional Component
+const Title = () => (
+    <h1 id="title" key="h2">Vanakkam ReactJs</h1>
+)
+//  JSX element
+const title = (
+    <h2>Javascript function in React Js</h2>
+)
+const HeaderComponent = () => {
+    return(
+        <>
+            {title}
+            <Title/>
+            {Title()}
+            <h2>This is React Js Learnig</h2>
+        </>
+    )
+}
 const root1 = ReactDOM.createRoot(document.getElementById("root"));
-root1.render(headingReact)
+root1.render(<HeaderComponent/>)
